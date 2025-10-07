3046c300ddceeaef06c3e9d1cb54421c03f66923
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2025-10-07 19:55:40 +0200

    Initial commit

---
+++ src/test/java/ai/synthai/businessbackend/BusinessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.synthai.businessbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
