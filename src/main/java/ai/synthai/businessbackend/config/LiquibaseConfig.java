9b57ebbda6a6d968f15eb4d15a61a2ba7932737e
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2025-10-16 11:48:57 +0200

    fix: removed unused imports

--- src/main/java/ai/synthai/businessbackend/config/LiquibaseConfig.java
+++ src/main/java/ai/synthai/businessbackend/config/LiquibaseConfig.java
@@ -6,14 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.ResourcePatternUtils;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.ResourceLoader;
 
 import liquibase.integration.spring.SpringLiquibase;
 
