8169bd9756421a50332672888dfe6a5f3182d27f
Jakub Kunikowski <kunikowskikuba@gmail.com>
2025-11-11 15:14:15 +0100

    refactor: TranscriptionResponseDto is now a generic class

--- src/main/java/ai/synthai/businessbackend/application/dto/TranscriptionResponseDto.java
+++ src/main/java/ai/synthai/businessbackend/application/dto/TranscriptionResponseDto.java
@@ -3,13 +3,12 @@ package ai.synthai.businessbackend.application.dto;
 import ai.synthai.businessbackend.domain.model.Category;
 import ai.synthai.businessbackend.domain.model.Language;
 import ai.synthai.businessbackend.domain.model.Status;
-import ai.synthai.businessbackend.domain.model.TranscriptionAnalysis;
 import lombok.Builder;
 
 @Builder
-public record TranscriptionResponseDto(
+public record TranscriptionResponseDto<T>(
         Status status,
-        TranscriptionAnalysis transcriptionAnalysis,
+        T transcriptionAnalysis,
         Category category,
         Float duration,
         Language language) {
