62d42ec52a798ccd9b6bd2ae7077dcffd0dc496f
Jakub Kunikowski <kunikowskikuba@gmail.com>
2025-11-11 14:11:41 +0100

    refactor: removed unnecessary packages

--- src/main/java/ai/synthai/businessbackend/domain/model/TranscriptionAnalysis.java
+++ src/main/java/ai/synthai/businessbackend/domain/model/TranscriptionAnalysis.java
@@ -1,11 +1,12 @@
 package ai.synthai.businessbackend.domain.model;
 
 import lombok.Builder;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 
 import java.util.Map;
 
 @Builder
+@Getter
 public class TranscriptionAnalysis {
     String transcription;
     Map<String, Object> summary;
