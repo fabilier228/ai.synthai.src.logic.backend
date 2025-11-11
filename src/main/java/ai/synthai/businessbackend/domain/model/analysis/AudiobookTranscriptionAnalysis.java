3d67326e7c0c3c1f88016518465427b78473392c
Jakub Kunikowski <kunikowskikuba@gmail.com>
2025-11-11 15:19:46 +0100

    feat: types for generic TranscriptionResponseDto

---
+++ src/main/java/ai/synthai/businessbackend/domain/model/analysis/AudiobookTranscriptionAnalysis.java
@@ -0,0 +1,8 @@
+package ai.synthai.businessbackend.domain.model.analysis;
+
+import ai.synthai.businessbackend.domain.model.analysis.summary.AudiobookSummary;
+
+public class AudiobookTranscriptionAnalysis {
+    String transcription;
+    AudiobookSummary summary;
+}
---
+++ src/main/java/ai/synthai/businessbackend/domain/model/analysis/CallTranscriptionAnalysis.java
@@ -0,0 +1,8 @@
+package ai.synthai.businessbackend.domain.model.analysis;
+
+import ai.synthai.businessbackend.domain.model.analysis.summary.CallSummary;
+
+public class CallTranscriptionAnalysis {
+    String transcription;
+    CallSummary summary;
+}
