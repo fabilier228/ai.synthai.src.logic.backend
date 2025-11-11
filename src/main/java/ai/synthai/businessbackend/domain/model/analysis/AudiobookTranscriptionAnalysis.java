4537891d93e50c97e71c6fadda3b2a69cfda7f28
Jakub Kunikowski <kunikowskikuba@gmail.com>
2025-11-11 15:54:03 +0100

    refactor: Builder annotation

--- src/main/java/ai/synthai/businessbackend/domain/model/analysis/AudiobookTranscriptionAnalysis.java
+++ src/main/java/ai/synthai/businessbackend/domain/model/analysis/AudiobookTranscriptionAnalysis.java
@@ -1,7 +1,12 @@
 package ai.synthai.businessbackend.domain.model.analysis;
 
 import ai.synthai.businessbackend.domain.model.analysis.summary.AudiobookSummary;
+import lombok.Builder;
+import lombok.Getter;
 
+
+@Getter
+@Builder
 public class AudiobookTranscriptionAnalysis {
     String transcription;
     AudiobookSummary summary;
