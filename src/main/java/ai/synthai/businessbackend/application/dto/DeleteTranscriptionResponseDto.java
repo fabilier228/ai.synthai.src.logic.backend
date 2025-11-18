cbe7bdfc5091abbc86c2e6febd1ca038f3e53496
Jakub Kunikowski <kunikowskikuba@gmail.com>
2025-11-18 13:06:44 +0100

    feat: added lombok annotation in order to help JSON deserialization

--- src/main/java/ai/synthai/businessbackend/application/dto/DeleteTranscriptionResponseDto.java
+++ src/main/java/ai/synthai/businessbackend/application/dto/DeleteTranscriptionResponseDto.java
@@ -1,9 +1,15 @@
 package ai.synthai.businessbackend.application.dto;
 
 import ai.synthai.businessbackend.domain.model.Status;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeleteTranscriptionResponseDto {
     Status status;
     String message;
--- src/main/java/ai/synthai/businessbackend/application/dto/KeycloakIdTranscriptionsDto.java
+++ src/main/java/ai/synthai/businessbackend/application/dto/KeycloakIdTranscriptionsDto.java
@@ -2,11 +2,17 @@ package ai.synthai.businessbackend.application.dto;
 
 import ai.synthai.businessbackend.domain.model.Status;
 import ai.synthai.businessbackend.domain.model.Transcription;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class KeycloakIdTranscriptionsDto {
     Status status;
     List<Transcription> transcriptions;
